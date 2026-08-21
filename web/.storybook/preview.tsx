9bd92ec2926d43890fd1c1bf492e3f19df1d7f23
Svetlana <svetlana.rashchupkina@gmail.com>
2026-08-21 11:01:01 +0200

    feat(web): enable a11y enforcement and autodocs in Storybook

--- web/.storybook/preview.tsx
+++ web/.storybook/preview.tsx
@@ -2,6 +2,7 @@ import type { Preview } from '@storybook/react-vite'
 import '../src/index.css'
 
 const preview: Preview = {
+  tags: ['autodocs'],
   parameters: {
     controls: {
       matchers: {
@@ -14,7 +15,7 @@ const preview: Preview = {
       // 'todo' - show a11y violations in the test UI only
       // 'error' - fail CI on a11y violations
       // 'off' - skip a11y checks entirely
-      test: 'todo'
+      test: 'error'
     }
   },
 };
