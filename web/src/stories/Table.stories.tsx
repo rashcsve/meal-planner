9e557d7c1fbdbcbd374bb304f95da5991f1f76b3
Svetlana <svetlana.rashchupkina@gmail.com>
2026-08-24 10:48:47 +0200

    refactor(recipes): replace side-effect ternary with if/else in Table story

--- web/src/stories/Table.stories.tsx
+++ web/src/stories/Table.stories.tsx
@@ -145,7 +145,11 @@ function FullTableStory() {
   function toggle(id: string | number) {
     setSelectedIds((prev) => {
       const next = new Set(prev);
-      next.has(id) ? next.delete(id) : next.add(id);
+      if (next.has(id)) {
+        next.delete(id);
+      } else {
+        next.add(id);
+      }
       return next;
     });
   }
